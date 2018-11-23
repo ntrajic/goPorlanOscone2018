3e403741aa0aae52e8bfe1ebb9a14f50d82d97e6
Nik Trajic <ntrajic@1090114bclt.onmicrosoft.com>
2018-11-23 11:18:23 -0800

    [SETUP]

---
+++ 2018-11-19-oreilly-online-trng-ntrajic/src/test/java/com/mattstine/lab/setup/HelloWorldTest.java
@@ -0,0 +1,25 @@
+package com.mattstine.lab.setup;
+
+import com.mattstine.lab.infrastructure.FirstSubmissionTest;
+import lombok.Value;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HelloWorldTest {
+
+    @Test
+    @Category(FirstSubmissionTest.class)
+    public void helloLabSubmission() {
+        HelloWorld helloWorld = new HelloWorld("It works!");
+
+        assertThat(helloWorld.getMessage()).isEqualTo("It works!");
+    }
+
+    @Value
+    private class HelloWorld {
+        String message;
+    }
+
+}
